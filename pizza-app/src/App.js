89a5c094c36cfde7ba8413cd535eefd994d02638
siroshd <dmytro.siroshtan@softec.sk>
2022-07-18 11:54:46 +0200

    add Cart and Header component

--- pizza-app/src/App.js
+++ pizza-app/src/App.js
@@ -4,6 +4,7 @@ import { Route, Routes } from 'react-router-dom';
 
 import Header from './components/Header';
 import Home from './pages/Home';
+import Cart from './pages/Cart';
 import NotFound from './pages/NotFound'
 
 import './scss/app.scss';
@@ -16,6 +17,8 @@ function App() {
       <Header/>
       <div className="content">
         <Routes>
+          <Route path={"/"} element={<Home/>}/>
+          <Route path={"/cart"} element={<Cart/>}/>
           <Route path={"*"} element={<NotFound/>}/>
         </Routes>
       </div>
